12fe438c748469827b0de34d68c07e2814d7ad99
dhruvdcoder <dhruvesp@bgl-dhruvesp-deb8-64>
2017-05-09 16:36:20 +0530

    LineInfo class created

--- include/LineInfo.h
+++ include/LineInfo.h
@@ -8,9 +8,22 @@
  */
 class LineInfo{
    private:
+	   Dwarf_Die m_die_ptr;
+	   Dwarf_Line* m_line_buffer_ptr;
+	   Dwarf_Line_Context m_line_context; /**< This is typedef struct Dwarf_Line_Context_s* . It lets us access line information convinitently. */
+	   Dwarf_Unsigned m_line_version;
+	   Dwarf_Unsigned m_table_count;
+      Dwarf_Signed m_line_count;
+
 
    public:
-	   getAddressFromLine(unsigned int line);
+	   // Constructors
+	   int setDiePtr(Dwarf_Die die_ptr);
+	   int setLineContextAndBuffer();
+	   //Dwarf_Addr getAddressFromLine(unsigned int line);
+
+      // destructors
+      ~LineInfo();
 }
 
 #endif // LINEINFO_H
---
+++ src/LineInfo.cpp
@@ -0,0 +1,56 @@
+/** @file LineInfo.cpp
+ * @breaf
+ */
+
+#include "../include/LineInfo.h"
+#include <stdio.h>
+
+/** @brief Sets the Line Context for the line info. This is used later in all member functions 
+ */
+
+int LineInfo::setLineContextAndBuffer(){
+   Dwarf_Error error;
+   if(m_die_ptr==nullptr){
+      fprintf(stderr,"Error: In LineInfo::setLineContextAndBuffer. 'm_die_ptr' not set.");
+      return DW_DLV_ERROR;
+   }
+   int res=dwarf_srclines_b(m_die_ptr,&m_line_version,&m_table_count,&m_line_contex,&error);
+   if(res != DW_DLV_OK){// DW_DLV_NO_ENTRY or DW_DLV_ERROR
+      fprintf(stderr,"Error: In LineInfo::setLineContextAndBuffer. In call to dwarf_srclines_b.");
+      return res;
+   }
+    if (m_table_count ==0){ // A line table with no entries.  
+      fprintf(stderr, "Warning: In LineInfo::setLineContextAndBuffer. A line table with no entries.");
+      // deallocate the memory
+      dwarf_srclines_dealloc_b(m_line_context);
+      m_line_context=nullptr;
+      m_line_buffer_ptr=nullptr;
+    } else if (m_table_count==1){
+        res=dwarf_srclines_from_linecontext(m_line_context,&m_line_buffer_ptr,m_line_count,error);
+   if(res != DW_DLV_OK){// DW_DLV_NO_ENTRY or DW_DLV_ERROR
+      fprintf(stderr,"Error: In LineInfo::setLineContextAndBuffer. In call to dwarf_srclines_from_linecontext.");
+      dwarf_srclines_dealloc_b(m_line_context);
+      m_line_context=nullptr;
+      m_line_buffer_ptr=nullptr;
+      return res;
+   }
+   return DW_DLV_OK;
+}
+
+/**@brief Set the reference CU DIE for the LineInfo object
+ */
+int LineInfo::setDiePtr(Dwarf_Die die_ptr){
+   m_die_ptr=die_ptr;
+   return DW_DLV_OK;
+}
+
+/** @brief Destructor to deallocate the memory managed as the client of libdwarf/
+ */
+LineInfo::~LineInfo(){
+   if(m_line_context){
+      Dwarf_Error error;
+      dwarf_srclines_dealloc_b(m_line_contex,&error);
+      }
+   m_line_buffer_ptr=nullptr;
+   m_die_ptr=nullptr; 
+}
