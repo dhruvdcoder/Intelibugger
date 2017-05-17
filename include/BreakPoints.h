4039ef90cc24db01b67696d357641306fab4c5d2
dhruvdcoder <dhruvesp@bgl-dhruvesp-deb8-64>
2017-05-17 06:07:54 +0530

    BreakPoints::add() added. Not tested

--- include/BreakPoints.h
+++ include/BreakPoints.h
@@ -5,6 +5,8 @@
 #ifndef _BREAKPOINTS_H
 #define _BREAKPOINTS_H 
 
+#include <vector>
+#include <stdint.h>
 
 /*! \class BreakPoints
  *  \brief The class works as a container for the breakpoints and hides the internal implentation of the storage.
@@ -14,10 +16,12 @@
 class BreakPoints
 {
 public:
-  void add(Dwarf_Unsigned lineno, Dwarf_Addr address, instruction);   
+  void add(Dwarf_Unsigned lineno, Dwarf_Addr address, uint64_t instruction);   
 
 private:
     struct bp; /*!< Struct nested inside the class. To not expose the impl and change it later. */
+    std::vector<bp> m_breakpoints;
+    static unsigned int m_s_number_of_idx;
 };
 
 #endif //_BREAKPOINTS_H
--- src/BreakPoints.cpp
+++ src/BreakPoints.cpp
@@ -3,6 +3,7 @@
  */
 #include "../include/BreakPoints.h"
 #include <string>
+
 struct BreakPoints::bp {
    Dwarf_addr m_bp_address;
    Dwarf_Unsigned m_bp_lineno;
@@ -14,4 +15,12 @@ struct BreakPoints::bp {
    }
    bpStatus m_bp_status;
    unsigned int m_bp_id;
+};
+
+
+/** @brief Function to add a breakpoint to the container
+ */
+void BreakPoints::add(Dwarf_Unsigned lineno, Dwarf_Addr address, uint64_t instruction) {
+   m_breakpoints.push_back(bp {address, lineno,"",instruction, bp_active,++m_s_number_of_idx});
+
 }
